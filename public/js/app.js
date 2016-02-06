c83c2f54e3879d1729d96f2779d524004bf6fe87
usmaks <anoly.konz.0099@gmail.com>
2016-02-06 23:50:48 +0900

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,19 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to socket.io server!');
 });
+
+socket.on('message', function (message) {
+  console.log('New message:');
+  console.log(message.text);
+});
+//
+// // Handles submitting of new message
+// var $form = jQuery('#message-form');
+//
+// $form.on('submit', function (event) {
+//   event.preventDefault();
+//
+//   socket.emit('message', {
+//     text: $form.find('input[name=message]').val()
+//   });
+// });
